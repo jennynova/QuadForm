24e591015eced7a1ba8d7f34ea186177b19ccf3f
Jenny Nova <jennynova@Jennys-MacBook-Pro-2.local>
2015-08-27 20:57:18 -0700

    Initial Commit

---
+++ QuadForm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuadForm
+//
+//  Created by Jenny Nova on 8/27/15.
+//  Copyright (c) 2015 Jenny Nova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
